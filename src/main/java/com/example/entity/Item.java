e0ab7601ee58804dcbd563f9fe96cb1d8b51ca50
YutaNobumoto <157275817+YutaNobumoto@users.noreply.github.com>
2024-02-22 11:08:33 +0900

    Lesson5実装途中

--- src/main/java/com/example/entity/Item.java
+++ src/main/java/com/example/entity/Item.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -26,21 +28,9 @@ public class Item {
 	@Column(name="PRICE")
 	private Integer price;
 	
-	@Column(name="DELETED_AT")
-	private LocalDateTime deletedAt;
-	
-	public LocalDateTime getDeletedAt() {
-		return this.deletedAt;
-	}
-	
-	public void setDeletedAt(LocalDateTime deletedAt) {
-		this.deletedAt=deletedAt;
-	}
-	
 	public Integer getId() {
 		return this.id;
 	}
-	
 	public void setId(Integer id) {
 		this.id=id;
 	}
@@ -48,7 +38,6 @@ public class Item {
 	public String getName() {
 		return this.name;
 	}
-	
 	public void setName(String name) {
 		this.name=name;
 	}
@@ -56,8 +45,34 @@ public class Item {
 	public Integer getPrice() {
 		return this.price;
 	}
-	
 	public void setPrice(Integer price) {
 		this.price=price;
 	}
+	
+	@Column(name="DELETED_AT")
+	private LocalDateTime deletedAt;
+	public LocalDateTime getDeletedAt() {
+		return this.deletedAt;
+	}
+	public void setDeletedAt(LocalDateTime deletedAt) {
+		this.deletedAt=deletedAt;
+	}
+	
+	@Column(name="CATEGORY_ID")
+	private Integer categoryId;
+	public Integer getCategoryId() {
+		return this.categoryId;
+	}
+	public void setCategoryId(Integer categoryId) {
+		this.categoryId=categoryId;
+	}
+	
+	@ManyToOne
+	@JoinColumn(name="CATEGORY_ID", insertable=false, updatable=false)
+	private Category category;
+	
+	public Category getCategory() {
+		return this.category;
+	}
+	
 }
